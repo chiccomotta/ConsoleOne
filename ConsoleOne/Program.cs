186b17ff2803d25266df738fa73c83e51deaa6b2
cristiano motta <cristiano.motta@hotmail.it>
2019-01-31 21:01:40 +0100

    patch commit

--- ConsoleOne/Program.cs
+++ ConsoleOne/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleOne
         static void Main(string[] args)
         {
             // First commit
+            // Patch commit
             Console.WriteLine("Hello World!");
         }
     }
